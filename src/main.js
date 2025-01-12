b3e2cd2d4d459c0abdb2b795acd77a9a6c764b6a
qiyan <qiyan@hey.com>
2025-01-12 22:43:05 +0800

    add build info

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, '../assets/icon.icns'),
+    title: 'mCommander',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
